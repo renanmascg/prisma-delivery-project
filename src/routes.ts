2998d2271c77203142869b153b65338278ff616d
renanmascg <renanmascg@gmail.com>
2022-05-03 11:09:07 -0300

    Cria a rota de criação de um entregador.

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/authenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/createClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/createDeliverymanController";
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 const routes = Router();
 
 routes.post("/client/", createClientController.handle);
+routes.post("/authenticate/", authenticateClientController.handle);
+routes.post("/deliveryman", createDeliverymanController.handle);
 
 export { routes }
